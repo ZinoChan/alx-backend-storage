6eed3cef3029c1519d6f7ceaf6afc62d46d0cf9c
ZinoChan <devzinochan@gmail.com>
2023-12-13 20:17:14 +0100

    fix not null

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 -- attr: id, email, name
 
 CREATE TABLE IF NOT EXISTS users {
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255),
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
 }

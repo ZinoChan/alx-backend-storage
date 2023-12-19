ed16033b732c97341a799b1c102ee60d8fe23282
Zino <72712106+ZinoChan@users.noreply.github.com>
2023-12-19 16:36:45 +0100

    fix order creteria

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,4 +4,4 @@
 SELECT
     band_name, (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%' ORDER BY DESC;
+WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;

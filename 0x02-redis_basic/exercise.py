daecbe13482f9bd6e1a4569948d12cd704e4a02a
ZinoChan <devzinochan@gmail.com>
2023-12-20 16:13:50 +0100

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,17 +3,32 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
+    """declare cache class"""
+
     def __init__(self):
         """init Redis client and flush db"""
-    self._redis = redis.Redis()
-    self._redis.flashdb()
+        self._redis = redis.Redis()
+        self._redis.flashdb()
 
-    def store(data: Union[str, float, bytes, int]) -> str:
+    def store(self, data: Union[str, float, bytes, int]) -> str:
         """set data in redis"""
         randKey = str(uuid.uuid4())
         self._redis.set(randKey, data)
         return randKey
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, float, bytes, int]:
+        value = self._redis.get(key)
+        if value:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str):
+        """Get data as str"""
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str):
+        return self.get(key, fn=int)
